7922910a19b61056aa492ec29fdf920b02276ab3
Leonardo <leonardo.rocha1990@gmail.com>
2015-09-17 13:35:15 -0500

    Adding method signatures, Also testing my first commit

--- src/CmdCommand.java
+++ src/CmdCommand.java
@@ -1,5 +1,21 @@
+import org.w3c.dom.Element;
+
 /**
  * Created by assar on 9/15/2015.
  */
 public class CmdCommand extends Command {
+	@Override
+	public String describe(){
+        return new String(); //Todo: description implementation goes here
+    }
+
+	@Override
+    public void execute(String workingDir){
+        /*todo: execution implementation goes here*/
+    }
+
+	@Override
+    public static Command parse(Element aElement){
+        return new Command(); //Todo: All parsing is done here somehow
+    }
 }
