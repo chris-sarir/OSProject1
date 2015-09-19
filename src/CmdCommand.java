784d3be8e7d2c1ebf804b2e863d2f9f87994e474
Silverpaladin123 <jce090020@utdallas.edu>
2015-09-19 15:38:33 -0500

    Updated Command() to contain a static map 'commandInfo' that holds 'id' and 'path' of all parsed commands from the batch file. Updated CmdCommand to parse the 'id', 'path', 'args', 'in', and 'out' attributes of each XML cmd command and put 'id' and 'path' into the 'commandInfo' Map. Updated WDCommand() and FileCommand() to parse the 'id' and 'path attributes of each corresponding XML command and put 'id' and 'path' into the 'commandInfo' Map.

--- src/CmdCommand.java
+++ src/CmdCommand.java
@@ -1,13 +1,24 @@
 import org.w3c.dom.Element;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /**
  * Created by assar on 9/15/2015.
  */
 public class CmdCommand extends Command
 {
+    private String id;
+    private String path;
+    private List<String> cmdArgs = new ArrayList<String>();
+    private String arg;
+    private String in;
+    private String out;
+
 	public String describe()
     {
-        return new String(); //Todo: description implementation goes here
+        return null; //Todo: description implementation goes here
     }
 
     public void execute(String workingDir)
@@ -17,6 +28,39 @@ public class CmdCommand extends Command
 
     public void parse(Element element)
     {
-        //Todo: All parsing is done here somehow
+        //get the 'id' from the XML line
+        id = element.getAttribute("id");
+        if (id == null || id.isEmpty()){
+            //Uncomment this line once we include catching exceptions
+            //throw new ProcessException("Missing ID in CMD Command");
+        }
+        //get the 'path' from the XML line
+        path = element.getAttribute("path");
+        if (path == null || path.isEmpty()){
+            //Uncomment this line once we include catching exceptions
+            //throw new Process Exception("Missing PATH in CMD Command");
+        }
+        //put the 'id' and 'path' into the abstract Command class shared Map
+        commandInfo.put(id, path);
+
+        //get the 'args' from the XML line
+        arg = element.getAttribute("args");
+        if (!(arg == null || arg.isEmpty())){
+            StringTokenizer st = new StringTokenizer(arg);
+            while (st.hasMoreTokens()){
+                String tok = st.nextToken();
+                cmdArgs.add(tok);
+            }
+        }
+        //get the "in" from the XML line
+        in = element.getAttribute("in");
+        if (!(in == null || in.isEmpty())){
+            //print 'in' or process exception
+        }
+        //get the "out" from the XML line
+        out = element.getAttribute("out");
+        if (!(out == null || out.isEmpty())){
+            //print 'out' or process exception
+        }
     }
 }
